e22201276f5b24e00a70d089d23cfb4890245af9
noobay <shadowswatcher@gmail.com>
2021-01-28 22:21:57 +0200

    Added time-point progression after a set duration.

--- Assets/Scripts/Mechanics/TimePassage.cs
+++ Assets/Scripts/Mechanics/TimePassage.cs
@@ -6,9 +6,45 @@ namespace Mechanics
     public class TimePassage : MonoBehaviour
     {
         [SerializeField]
+        private float m_progressDurationSeconds;
+
+        [SerializeField]
         private List<TimePoint> m_timePoints;
 
-        public TimePoint ActiveTimePoint { get; set; }
+        private int m_activeTimePointIndex;
+
+        private float m_lastPointTime;
+
+        public TimePoint ActiveTimePoint
+        {
+            get
+            {
+                return m_timePoints[m_activeTimePointIndex];
+            }
+        }
+
+        public void ProgressTime(int progressBy)
+        {
+            m_activeTimePointIndex += progressBy;
+        }
+
+        private void Start()
+        {
+            m_lastPointTime = Time.time;
+        }
+
+        private void Update()
+        {
+            if( Time.time - m_lastPointTime > m_progressDurationSeconds)
+            {
+                m_lastPointTime = Time.time;
+
+                if (m_activeTimePointIndex < m_timePoints.Count)
+                {
+                    ProgressTime(1);
+                }
+            }
+        }
     }
 
     public enum ETimeEffect
