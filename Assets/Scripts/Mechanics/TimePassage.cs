62911046caf3aa0c808a37f9e7bfb3709ae40313
noobay <shadowswatcher@gmail.com>
2021-01-28 21:54:40 +0200

    Added a TimePassage script to manage the passage of time. Also causes various effects along the timeline.

---
+++ Assets/Scripts/Mechanics/TimePassage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mechanics
+{
+    public class TimePassage : MonoBehaviour
+    {
+        [SerializeField]
+        private List<TimePoint> m_timePoints;
+    }
+
+    enum ETimeEffect
+    {
+        NONE,
+        QUARANTINE_START,
+        QUARANTINE_END,        
+    }
+
+    [System.Serializable]
+    struct TimePoint
+    {
+        public string label;
+        public ETimeEffect timeEffect;
+    }
+}
