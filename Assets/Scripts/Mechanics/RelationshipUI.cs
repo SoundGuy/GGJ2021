61f752f9dc3e3c46fd9b6bae8bf3517da2c32c83
Oded Sharon <github@odedsharon.com>
2021-01-29 11:13:34 +0200

    added TMP added base for friends mechanics

---
+++ Assets/Scripts/Mechanics/RelationshipUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class RelationshipUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro indicator;
+
+    public void UpdateIndicator(float value)
+    {
+        indicator.text = value.ToString("00");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!indicator)
+        {
+            indicator = GetComponentInChildren<TextMeshPro>();
+            if (!indicator)
+            {
+                Debug.LogError("No Indicator!");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
