ab76f7212c5c85d85d4d38ec9b0dad135bca00dc
noobay <shadowswatcher@gmail.com>
2021-01-29 13:40:53 +0200

    Added a roomsManager. To manage room traversal and time passage.

---
+++ Assets/Scripts/Mechanics/RoomsManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Mechanics
+{
+    public class RoomsManager : MonoBehaviour
+    {
+        public int RoomsVisited { get; private set; }
+
+        [SerializeField]
+        private float secondsPassedPerRoom = 50f;
+
+        [SerializeField]
+        private TimePassage timePassage;
+
+        public void UserVisitedRoom()
+        {
+            RoomsVisited++;
+
+            timePassage.ProgressTime(secondsPassedPerRoom);
+        }
+    }
+}
+
