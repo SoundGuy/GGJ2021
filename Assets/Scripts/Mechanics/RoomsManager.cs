f343e8a9890c8763363bc04c03d92107fd49caec
noobay <shadowswatcher@gmail.com>
2021-01-30 03:11:58 +0200

    RoomsMangager now loads the home room when awake

--- Assets/Scripts/Mechanics/RoomsManager.cs
+++ Assets/Scripts/Mechanics/RoomsManager.cs
@@ -8,11 +8,28 @@ namespace Mechanics
         public Action<ERoomID> OnRoomVisited;
         public int RoomsVisited { get; private set; }
 
+        [SerializeField]
+        private string firstRoom = "Home";
+
+        SceneLoadManager sceneLoadManager;
+
         public void UserVisitedRoom(ERoomID roomId)
         {
             RoomsVisited++;
 
             OnRoomVisited?.Invoke(roomId);
+        }
+
+        private void Awake()
+        {
+            sceneLoadManager = FindObjectOfType<SceneLoadManager>();
+
+            if (sceneLoadManager == null)
+            {
+                throw new System.Exception("Missing a SceneLoadManager in the scene");
+            }
+
+            sceneLoadManager.ChangeScene(firstRoom);
         }
     }
 
